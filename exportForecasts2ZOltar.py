02597851d6629f52697a03f1a3e9f30dc4abfd18
tom <tomcm39@gmail.com>
2019-12-18 10:33:21 -0500

    update zoltar code

--- exportForecasts2ZOltar.py
+++ exportForecasts2ZOltar.py
@@ -7,8 +7,23 @@ import os
 from glob import glob
 import re
 
+def connect2Zoltar:
+    env_user='Z_USERNAME'
+    env_pass='Z_PASSWORD'
+    conn = ZoltarConnection()
+    conn.authenticate(os.environ.get(env_user),os.environ.get(env_pass))
+    return conn
+    
+def reformatDate(x):
+    year,month,day = x.split('-')
+    year  = int(year)
+    month = int(month)
+    day   = int(day)
+    return "{:04d}-{:02d}-{:02d}".format(year,month,day)
+
 def grabForecastWasSubmitted(fl):
     date = re.findall('\d{4}-\d{2}-\d+',fl)[0]
+    date = reformatDate(date)
     return ''.join(date.split('-'))
 
 def grabEW(fl):
@@ -31,25 +46,29 @@ def relateEW2SubmissionFiles():
 
 if __name__ == "__main__":
 
-    env_user='Z_USERNAME'
-    env_pass='Z_PASSWORD'
-    conn = ZoltarConnection()
-    conn.authenticate(os.environ.get(env_user)
-                  ,os.environ.get(env_pass))
-    
+    conn = connect2Zoltar()
+   
     project_name = 'CDC Real-time Forecasts'
     model_name   = 'KoT-adaptive'
 
-    EW2submitDates = sortDateOfSubmissions()
-    EW2submissionFile = relateEW2SubmissionFiles() 
+    EW2submitDates    = sortDateOfSubmissions()
+    EW2submissionFile = relateEW2SubmissionFiles()
+
+    EW2submitDates = { 'EW42':'20191014'
+                      ,'EW43':'20191021'
+                      ,'EW44':'20191028'
+                      ,'EW45':'20191104'
+                      ,'EW46':'20191111'
+                      ,'EW47':'20191118'}
 
     for EW in EW2submitDates:
-#        try:
-        timezero_date = EW2submitDates[EW]
-        forecast_file_path = EW2submissionFile[EW]
-        predx_json, forecast_filename = util.convert_cdc_csv_to_json_io_dict(forecast_file_path)
-        conn = util.authenticate()
-        util.upload_forecast(conn, predx_json, forecast_filename, project_name, model_name, timezero_date, overwrite=True)
-        print('uploaded {:s}'.format(forecast_file_path))
-#        except:
-#            print('error uploading {:s}'.format(forecast_file_path))
+        try:
+            timezero_date      = EW2submitDates[EW]
+            forecast_file_path = EW2submissionFile[EW]
+            predx_json, forecast_filename = util.convert_cdc_csv_to_json_io_dict(forecast_file_path)
+            conn = util.authenticate()
+            util.upload_forecast(conn, predx_json, forecast_filename, project_name, model_name, timezero_date, overwrite=True)
+            print('uploaded {:s}'.format(forecast_file_path))
+        except Exception as e:
+            print('error uploading {:s}'.format(forecast_file_path))
+            print('{:s}'.format(str(e)))
